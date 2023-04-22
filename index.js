43bb872c5562183763fb264ba10219728d5e7f53
lithium <code@hlidskialf.com>
2023-04-21 18:15:43 -0700

    scaffold scanner and html output

--- index.js
+++ index.js
@@ -1,19 +1,64 @@
 
 const Handlebars = require("handlebars");
-const fs = require('fs')
+const fs = require('fs-extra')
 const path = require('path')
-
-//const template = Handlebars.compile("Name: {{name}}");
-// console.log(template({ name: "Nils" }));
-// console.log(process.argv)
-
 
 async function main() {
 	const archive_path = "archive"
 	const output_path = "build"
+	const template_dir = "templates"
 
-	const archive_context = await read_collections(archive_path)
-	console.log(archive_context)
+	const collections = await read_collections(archive_path)
+
+	console.log("collections",collections)
+
+	await render_template(
+		path.join(template_dir, 'index.html'),
+		path.join(output_path, 'index.html'), 
+		{
+			page_title: "Eco Law Archive",
+			collections: collections,
+		}
+	)
+
+	collections.forEach(async (collection) => {
+		await render_template(
+			path.join(template_dir, 'collection.html'),
+			path.join(output_path, 'collections', collection.name+'.html'), 
+			{
+				page_title: "Eco Law Archive",
+				collection,
+			}
+		)
+
+
+
+		collection.sets.forEach(async (set) => {
+			await render_template(
+				path.join(template_dir, 'set.html'),
+				path.join(output_path, 'collections', collection.name, set.name+'.html'), 
+				{
+					page_title: "Eco Law Archive",
+					set,
+				}
+			)
+
+
+		})
+
+
+	})
+}
+
+async function render_template(template_path, output_path, context) {
+	const template_txt = (await fs.promises.readFile(template_path)).toString()
+
+	const template = await Handlebars.compile(template_txt)
+	const rendered = template(context)
+
+	console.log("Writing output", output_path)
+	await fs.ensureDir(path.dirname(output_path))
+	await fs.promises.writeFile(output_path, rendered.toString())
 }
 
 async function read_collections(archive_path) {
@@ -41,18 +86,21 @@ async function read_collections(archive_path) {
 
 	const archive_dir = await fs.promises.opendir(archive_path)
 	for await (const collection_entry of archive_dir) {
+		const collection_path = path.join(archive_path, collection_entry.name)
+		console.log("Scanning collection", collection_path)
+
 		const collection = {
 			name: collection_entry.name,
 			sets: []
 		}
-
-		const collection_path = path.join(archive_path, collection_entry.name)
 		const collection_dir = await fs.promises.opendir(collection_path)
 		for await (const set_entry of collection_dir) {
 			const set_path = path.join(collection_path, set_entry.name)
+			console.log("Scanning set", set_path)
 
 			const set = await read_civics_directory(set_path)
 			set.name = set_entry.name
+			set.collection_name = collection.name
 			collection.sets.push(set)
 		}
 		output.push(collection)
@@ -67,10 +115,12 @@ async function read_civics_directory(directory_path) {
 
 	for await (const entry of dir) {
 		entry_path = path.join(directory_path, entry.name)
+		console.log("Scanning json", entry_path)
+
 		const civics_txt = await fs.promises.readFile(entry_path)
 		const civics_obj = JSON.parse(civics_txt)
 
-		const type = civics_obj.type
+		const type = civics_obj.type.split('.').pop()
 		if (!(type in output)) {
 			output[type] = []
 		}
